b5254fc737c10974db3f233fb95c18d7e637bf72
Roger Hyam <rhyam@rbge.org.uk>
2022-08-28 10:27:41 +0100

    Added perfs

--- perf_offset.py
+++ perf_offset.py
@@ -1,34 +1,57 @@
 
+import os
+from PIL import Image, ImageStat, ImageChops
 
-from PIL import Image
-
-test_file_path = "images/2022-08-26_012.jpg"
-width_offset = 0.1  # percentage of the width of image
-window_size = 100  # size of the sampling box (pixels each side)
+directory = "images"
 
 
-# Open image using Image module
-img = Image.open(test_file_path)
-# Show actual Image
-# im.show()
+def process_image(file_path):
 
-width, height = img.size
+    width_offset = 0.1  # percentage of the width of image
+    height_offset = 0.1
+    window_size = 100  # size of the sampling box (pixels each side)
 
-top_left_x = width_offset * width
-top_left_y = 0
+    # Open image using Image module
+    img = Image.open(file_path)
+    # Show actual Image
+    # im.show()
 
-bottom_right_x = top_left_x + window_size
-bottom_right_y = top_left_y + window_size
+    width, height = img.size
 
-window = (top_left_x, top_left_y, bottom_right_x, bottom_right_y)
-cropped_img = img.crop(window).convert('L')
-stat = ImageStat.Stat(cropped_img)
+    # cropped_img.show()
 
-print('banana')
-print(stat.mean[0])
+    # loop down the image
+    perf_start_y = 100
+    for y in range(height):
 
-# cropped_img.show()
+        # window
+        top_left_x = width_offset * width
+        top_left_y = y
+        bottom_right_x = top_left_x + window_size
+        bottom_right_y = top_left_y + window_size
+        window = (top_left_x, top_left_y, bottom_right_x, bottom_right_y)
 
-# loop down the image
-# for i in range(height):
-#    print(height * i)
+        # get that window
+        cropped_img = img.crop(window).convert('L')
+        stat = ImageStat.Stat(cropped_img)
+        avg = stat.mean[0]
+
+        if avg == 255:
+            if y > perf_start_y:
+                shift_y = (y - perf_start_y)
+            else:
+                shift_y = perf_start_y - y
+            break
+
+    img = ImageChops.offset(img, 0, shift_y * -1)
+    img.save("3_" + file_path)
+    # img.show()
+
+
+for filename in os.listdir(directory):
+    if filename == ".DS_Store":
+        continue
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        process_image(f)
